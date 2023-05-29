6e0214e2834225458fea1f5669e523c9751e38fb
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2023-05-29 16:32:40 -0300

    User Service and User Repository

--- demo/src/main/java/br/com/Gabriel/controller/UserController.java
+++ demo/src/main/java/br/com/Gabriel/controller/UserController.java
@@ -1,5 +1,6 @@
 package br.com.Gabriel.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,15 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.Gabriel.domain.User;
+import br.com.Gabriel.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
-
+	
+	@Autowired
+	private UserService service;
+	
+	
 	@GetMapping(value = "/{id}")
 	public  ResponseEntity<User> findById(@PathVariable Long id){
 		
-		return ResponseEntity.ok().body(new User(1L, "Gabriel", "gabrielfneves2001@gmail.com", "1234"));
+		return ResponseEntity.ok().body(service.findById(id));
 		
 	}
 	
