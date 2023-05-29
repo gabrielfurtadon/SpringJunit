d8e5b397e4a4162cbe993ad5fd1588993d2c478c
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2023-05-29 16:13:43 -0300

    entity User

--- demo/src/main/java/br/com/Gabriel/domain/User.java
+++ demo/src/main/java/br/com/Gabriel/domain/User.java
@@ -1,5 +1,33 @@
 package br.com.Gabriel.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity(name = "tb_user")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	@Column(unique = true)
+	private String email;
+	private String password;
+	
+	
+	
+	
 }
